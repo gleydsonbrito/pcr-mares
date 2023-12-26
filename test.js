8118ed6cdc1a220ba0cae6916e458c4e57603802
gleydsonbrito <gleydsonbrito@hotmail.com>
2023-12-26 15:46:52 -0300

    add temperature featur fix api

--- test.js
+++ test.js
@@ -1,10 +1,26 @@
-const lat = 60.936;
-const lng = 5.114;
+const username='pcr_brito_gleydson'
+const password='kL4r7CrDC6'
+let headers = new Headers();
+headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
 
-fetch(`https://api.stormglass.io/v2/tide/extremes/point?lat=${lat}&lng=${lng}&start=2023-12-11&end=2023-12-11`, {
-  headers: {
-    'Authorization': '4e85d9ca-983b-11ee-b7a8-0242ac130002-4e85da2e-983b-11ee-b7a8-0242ac130002'
-  }
-}).then((response) => response.json()).then((jsonData) => {
-    console.log(jsonData)
-});
+const day = new Date()
+
+
+fetch('https://login.meteomatics.com/api/v1/token', {
+  method: 'GET', headers:headers,
+})
+  .then(res => res.json())
+  .then(data => data.access_token)
+  .then( token => {
+    const endpoint = `https://api.meteomatics.com/${day.toISOString()}/t_2m:C,t_max_2m_24h:C,t_min_2m_24h:C,uv:idx,sunrise:sql,sunset:sql/-8.053967501715206,-34.87222573396811/json?access_token=${token}`
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(res => {
+        const currentTemp = res.data[0].coordinates[0].dates[0].value
+        const maxTemp = res.data[1].coordinates[0].dates[0].value
+        const minTemp = res.data[2].coordinates[0].dates[0].value
+        const uv = res.data[3].coordinates[0].dates[0].value
+        const sunrise = res.data[4].coordinates[0].dates[0].value
+        const sunset = res.data[5].coordinates[0].dates[0].value
+      })
+  })
