a7fae1037de448386ab9cabcbb41ed37ae75205a
gleydsonbrito <gleydsonbrito@hotmail.com>
2024-01-08 09:39:48 -0300

    fix offline info for UV card

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log(new Date(1703214000000))
+const url = 'https://open-weather13.p.rapidapi.com/city/recife';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5b155a0580msh3ab87547eb8f3cfp1f3b29jsnd9733114bb0a',
+		'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+	}
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(res => console.log(res))
